114444a44008131599a08d4375691dbc411730e1
demchuknaz <Demchuknaz@gmail.com>
2026-02-15 17:21:33 +0100

    updated with Fix multiple columns at once

--- Project_CleanAndSplit_banking_data.py
+++ Project_CleanAndSplit_banking_data.py
@@ -1,6 +1,3 @@
-Project cleaning the banking data + split the results
-
-
 import pandas as pd
 import numpy as np
 
@@ -18,4 +15,12 @@ duplicates = bank_marketingDF.duplicated(subset = ["client_id"], keep= False)
 print(duplicates)
 print(f"Total rows involved in duplicates: {duplicates.sum()}")
 
+# Fix multiple columns at once
+cols_to_fix = ['job', 'education']
 
+for col in cols_to_fix:
+    bank_marketingDF[col] = bank_marketingDF[col].str.replace('.', '_', regex=False)
+
+bank_marketingDF['education'] = bank_marketingDF['education'].replace('unknown', np.NaN)
+    
+print(bank_marketingDF.head(50))
