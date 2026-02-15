af475709fea369a8ac2cc668d34762a9b0d5888a
demchuknaz <Demchuknaz@gmail.com>
2026-02-15 17:00:17 +0100

    Init file Project_CleanAndSplit_banking_data.py

---
+++ Project_CleanAndSplit_banking_data.py
@@ -0,0 +1,21 @@
+Project cleaning the banking data + split the results
+
+
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+bank_marketingDF = pd.read_csv('bank_marketing.csv')
+
+print(bank_marketingDF.head(15))
+print(bank_marketingDF.dtypes)
+
+# Client's type of job	Change "." to "_"
+
+# Check if there are any duplicates and print a sum of them
+
+duplicates = bank_marketingDF.duplicated(subset = ["client_id"], keep= False)
+print(duplicates)
+print(f"Total rows involved in duplicates: {duplicates.sum()}")
+
+
